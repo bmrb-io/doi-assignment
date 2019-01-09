f8ac86c0a2b7a3b7e75b0595c76b0ce077d0d77e
Jon Wedell <wedell@bmrb.wisc.edu>
2019-01-09 12:52:31 -0600

    Make a certain error not require verbose

--- assign.py
+++ assign.py
@@ -231,5 +231,4 @@ if __name__ == "__main__":
             try:
                 session.create_or_update_doi(an_entry)
             except IOError:
-                if options.verbose:
-                    print("Skipping entry that isn't in redis yet: %s" % an_entry)
+                print("Skipping entry that isn't in redis yet (did the DB reloader script run yet?): %s" % an_entry)
