a198968e5d6b3ba72da8b454579c6c689a49676e
Jon Wedell <jonwedell@gmail.com>
2019-12-04 13:20:28 -0600

    Tweak initial virtualenv setup script.

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python -m virtualenv env
-source ./env/bin/activate
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+python -m virtualenv ${SCRIPT_DIR}/env
+source ${SCRIPT_DIR}/env/bin/activate
 pip install -r requirements.txt
