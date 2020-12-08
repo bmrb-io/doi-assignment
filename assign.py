5c188eda5d2cac6fa0064089169bfdea6c9d1627
jon@jonwedell.com <f6ac4ceae0ad45089108fb1ed28d28b441a0866a>
2020-12-08 08:22:40 -0600

    Fix bug in previous commit to DOI assignment script

--- assign.py
+++ assign.py
@@ -149,13 +149,14 @@ class EZIDSession:
 
         # Get the authors - with middle initial if possible, but if not, just use first and last
         try:
-            authors = ent.get_tags(['_Entry_author.Family_name', '_Entry_author.Given_name',
-                                    '_Entry_author.Middle_initials'])
+            authors = ent.get_loops_by_category('entry_author')[0].filter(
+                ['_Entry_author.Family_name', '_Entry_author.Given_name', '_Entry_author.Middle_initials']).data
             for auth in authors:
                 if auth[2] and auth[2] != ".":
                     auth[1] += " " + auth[2]
         except KeyError:
-            authors = ent.get_tags(['_Entry_author.Family_name', '_Entry_author.Given_name'])
+            authors = ent.get_loops_by_category('entry_author')[0].filter(
+                ['_Entry_author.Family_name', '_Entry_author.Given_name']).data
         for auth in authors:
             creator = eTree.SubElement(creators, "creator")
             creator_name = eTree.SubElement(creator, 'creatorName')
