bd7309f4d5f8714b27acc38510cc85ca7c3febab
jon@jonwedell.com <f6ac4ceae0ad45089108fb1ed28d28b441a0866a>
2021-04-08 13:18:30 -0500

    Update DOI assignment tool to use JSON api rather than old deprecated EZID one.

--- assign.py
+++ assign.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """ Script to assign the DOIs. """
-
+import base64
 import json
 import logging
 import multiprocessing
@@ -16,174 +16,186 @@ import psycopg2
 import pynmrstar
 import requests
 
-import anvl
+# from datacite import DataCiteMDSClient
 
 config_file = os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__))), 'configuration.json')
 config = json.load(open(config_file, 'r'))
-ezid_base = config['ezid_base']
-ezid_username = config['ezid_username']
-ezid_password = config['ezid_password']
+base_url = config['base_url']
+username = config['username']
+password = config['password']
 shoulder = config['shoulder']
 
 
-def assign_id(entry_id):
-    with EZIDSession() as EZID:
-        EZID.create_or_update_doi(entry_id)
+def get_id(doi):
+    """ Returns the information about a DOI."""
+    r = requests.get("%s/dois/%s" % (base_url, doi))
+    r.raise_for_status()
+    return r.json()
 
 
-class EZIDSession:
-    """ A session with the EZID server."""
+def determine_doi(entry):
+    """ Determines the DOI for an entry."""
 
-    def determine_doi(self, entry):
-        """ Determines the DOI for an entry."""
+    if entry.startswith("bmse") or entry.startswith("bmst"):
+        return '%s%s' % (shoulder, entry.upper())
+    elif entry.startswith("bmr"):
+        return '%s%s' % (shoulder, entry.upper())
+    else:
+        return '%sBMR%s' % (shoulder, entry)
 
-        if entry.startswith("bmse") or entry.startswith("bmst"):
-            return '%s%s' % (shoulder, entry.upper())
-        elif entry.startswith("bmr"):
-            return '%s%s' % (shoulder, entry.upper())
+
+def determine_entry_url(entry, data_type='string') -> str:
+    """ Determines the location for an entry."""
+
+    if data_type == 'string':
+        if entry.startswith("bmse"):
+            return f'https://bmrb.io/metabolomics/mol_summary/show_data.php?id={entry}'
+        elif entry.startswith("bmst"):
+            return f'https://bmrb.io/metabolomics/mol_summary/show_theory.php?id={entry}'
         else:
-            return '%sBMR%s' % (shoulder, entry)
+            return f'https://bmrb.io/data_library/summary/?bmrbId={entry}'
+    elif data_type == 'star':
+        if entry.startswith("bmse") or entry.startswith("bmst"):
+            return f'https://bmrb.io/ftp/pub/bmrb/metabolomics/entry_directories/{entry}/{entry}.str'
+        else:
+            return f'https://bmrb.io/ftp/pub/bmrb/entry_directories/bmr{entry}/bmr{entry}_3.str'
 
-    def get_id(self, doi):
-        """ Returns the information about a DOI."""
 
-        r = requests.get("%s/metadata/%s" % (ezid_base, doi),
-                         auth=(ezid_username, ezid_password))
-        r.raise_for_status()
+def withdraw(entry):
+    """ Withdrawn an entry. """
 
-        return r.text
+    doi = determine_doi(entry)
+    url = f"{base_url}/dois/{doi}"
+    full_data = {
+        "data": {
+            "id": doi,
+            "type": "dois",
+            "attributes": {
+                "event": "hide",
+                "doi": doi
+            }
+        }
+    }
+    r = requests.put(url, json=full_data, auth=(username, password),
+                     headers={'Content-Type': 'application/vnd.api+json'})
+    if r.status_code < 300:
+        logging.info("Withdrew entry: %s" % entry)
+    r.raise_for_status()
 
-    def withdraw(self, entry):
-        """ Withdrawn an entry. """
 
-        doi = self.determine_doi(entry)
-        url = "https://ez.datacite.org/id/%s" % doi
-        r = requests.post(url,
-                          data=anvl.escape_dictionary({'_status': 'unavailable | withdrawn by author'}),
-                          auth=(ezid_username, ezid_password),
-                          headers={'Accept': 'text/plain', 'Content-Type': 'text/plain'})
+def get_entry_metadata(entry) -> str:
+    """ Returns a base-64 encoded XML string with all of the known information about
+    an entry."""
 
-        if r.status_code < 300:
-            logging.info("Withdrew entry: %s" % entry)
-        r.raise_for_status()
+    try:
+        ent = pynmrstar.Entry.from_database(entry)
+    except ValueError as err:
+        raise ValueError("Something went wrong when getting an entry (%s) from the database: %s" % (entry, err))
+    # Get the data we will need
+    release_loop = ent.get_loops_by_category('release')[0]
+    release_loop.sort_rows('release_number')
+    release_loop.add_missing_tags()
+    release_loop = release_loop.get_tag(['date', 'detail'])
 
-    def create_or_update_doi(self, entry, timeout=1):
-        """ Assign the metadata, then update the URL/release the DOI if not yet created. """
+    root = eTree.Element("resource")
+    root.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+    root.set("xmlns", 'http://datacite.org/schema/kernel-4')
+    root.set("xsi:schemaLocation",
+             "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd")
 
-        timeout = timeout * 2
-        try:
-            entry_meta = self.get_entry_metadata(entry)
-            doi = self.determine_doi(entry)
+    identifier = eTree.SubElement(root, "identifier")
+    identifier.set("identifierType", 'DOI')
+    identifier.text = determine_doi(entry)
 
-            # First create the metadata
-            url = "%s/metadata/%s" % (ezid_base, doi)
-            r = requests.put(url, data=entry_meta,
-                             headers={'Content-Type': 'application/xml'},
-                             auth=(ezid_username, ezid_password))
+    # Get the authors in the form Last Name, Middle Initial, First Name,;
+    creators = eTree.SubElement(root, "creators")
 
-            r.raise_for_status()
-
-            url = "%s/doi/%s" % (ezid_base, doi)
-            if entry.startswith("bmse"):
-                content_url = 'https://bmrb.io/metabolomics/mol_summary/show_data.php?id=%s' % entry
-            elif entry.startswith("bmst"):
-                content_url = 'https://bmrb.io/metabolomics/mol_summary/show_theory.php?id=%s' % entry
-            else:
-                content_url = 'https://bmrb.io/data_library/summary/?bmrbId=%s' % entry
-            release_string = "doi=%s\nurl=%s" % (doi, content_url)
-
-            r = requests.put(url, data=release_string,
-                             headers={'Content-Type': 'text/plain'},
-                             auth=(ezid_username, ezid_password))
-            r.raise_for_status()
-
-            logging.info("Created or updated entry: %s" % entry)
-        except requests.HTTPError as e:
-            logging.warning("A HTTP exception occurred for entry %s: %s" % (entry, e))
-            logging.info("Trying again...")
-            if timeout <= 128:
-                time.sleep(timeout)
-                return self.create_or_update_doi(entry, timeout)
-            else:
-                logging.error('Multiple attempts to assign entry %s failed.' % entry)
-        except IOError as err:
-            logging.exception("Entry %s not loaded in the DB: %s" % (entry, err))
-        finally:
-            time.sleep(1)
-
-    def get_entry_metadata(self, entry):
-        """ Returns a python dict with all of the known information about
-        an entry."""
-
-        try:
-            ent = pynmrstar.Entry.from_database(entry)
-        except ValueError as err:
-            raise ValueError("Something went wrong when getting an entry (%s) from the database: %s" % (entry, err))
-        # Get the data we will need
-        release_loop = ent.get_loops_by_category('release')[0]
-        release_loop.sort_rows('release_number')
-        release_loop.add_missing_tags()
-        release_loop = release_loop.get_tag(['date', 'detail'])
-
-        root = eTree.Element("resource")
-        root.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-        root.set("xmlns", 'http://datacite.org/schema/kernel-4')
-        root.set("xsi:schemaLocation",
-                 "http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd")
-
-        identifier = eTree.SubElement(root, "identifier")
-        identifier.set("identifierType", 'DOI')
-        identifier.text = self.determine_doi(entry)
-
-        # Get the authors in the form Last Name, Middle Initial, First Name,;
-        creators = eTree.SubElement(root, "creators")
-
-        # Get the authors - with middle initial if possible, but if not, just use first and last
-        try:
-            authors = ent.get_loops_by_category('entry_author')[0].filter(
-                ['_Entry_author.Family_name', '_Entry_author.Given_name', '_Entry_author.Middle_initials']).data
-            for auth in authors:
-                if auth[2] and auth[2] != ".":
-                    auth[1] += " " + auth[2]
-        except (ValueError, KeyError):
-            authors = ent.get_loops_by_category('entry_author')[0].filter(
-                ['_Entry_author.Family_name', '_Entry_author.Given_name']).data
+    # Get the authors - with middle initial if possible, but if not, just use first and last
+    try:
+        authors = ent.get_loops_by_category('entry_author')[0].filter(
+            ['_Entry_author.Family_name', '_Entry_author.Given_name', '_Entry_author.Middle_initials']).data
         for auth in authors:
-            creator = eTree.SubElement(creators, "creator")
-            creator_name = eTree.SubElement(creator, 'creatorName')
-            creator_name.text = ", ".join([auth[0], auth[1]])
-            creator_name.set('nameType', 'Personal')
+            if auth[2] and auth[2] != ".":
+                auth[1] += " " + auth[2]
+    except (ValueError, KeyError):
+        authors = ent.get_loops_by_category('entry_author')[0].filter(
+            ['_Entry_author.Family_name', '_Entry_author.Given_name']).data
+    for auth in authors:
+        creator = eTree.SubElement(creators, "creator")
+        creator_name = eTree.SubElement(creator, 'creatorName')
+        creator_name.text = ", ".join([auth[0], auth[1]])
+        creator_name.set('nameType', 'Personal')
 
-            # Datacite doesn't like it when we provide these...
-            # if auth[0]:
-            #   eTree.SubElement(creator, 'familyName').text = auth[0]
-            # if auth[1] and auth[2]:
-            #   eTree.SubElement(creator, 'givenName').text = auth[1] + " " + auth[2]
-            # elif auth[1]:
-            #   eTree.SubElement(creator, 'givenName').text = auth[1]
+        # Datacite doesn't like it when we provide these...
+        # if auth[0]:
+        #   eTree.SubElement(creator, 'familyName').text = auth[0]
+        # if auth[1] and auth[2]:
+        #   eTree.SubElement(creator, 'givenName').text = auth[1] + " " + auth[2]
+        # elif auth[1]:
+        #   eTree.SubElement(creator, 'givenName').text = auth[1]
 
-        titles = eTree.SubElement(root, "titles")
-        eTree.SubElement(titles, "title").text = ent.get_tag("entry.title")[0].replace("\n", "")
-        eTree.SubElement(root, "publisher").text = 'Biological Magnetic Resonance Bank'
-        eTree.SubElement(root, "publicationYear").text = release_loop[0][0][:4]
-        resource_type = eTree.SubElement(root, 'resourceType')
+    titles = eTree.SubElement(root, "titles")
+    eTree.SubElement(titles, "title").text = ent.get_tag("entry.title")[0].replace("\n", "")
+    eTree.SubElement(root, "publisher").text = 'Biological Magnetic Resonance Bank'
+    eTree.SubElement(root, "publicationYear").text = release_loop[0][0][:4]
+    resource_type = eTree.SubElement(root, 'resourceType')
 
-        dates = eTree.SubElement(root, 'dates')
-        release_date = eTree.SubElement(dates, 'date')
-        release_date.set('dateType', 'Available')
-        if release_loop[0][1] and release_loop[0][1] not in pynmrstar.definitions.NULL_VALUES:
-            release_date.set('dateInformation', release_loop[0][1])
-        release_date.text = release_loop[0][0]
+    dates = eTree.SubElement(root, 'dates')
+    release_date = eTree.SubElement(dates, 'date')
+    release_date.set('dateType', 'Available')
+    if release_loop[0][1] and release_loop[0][1] not in pynmrstar.definitions.NULL_VALUES:
+        release_date.set('dateInformation', release_loop[0][1])
+    release_date.text = release_loop[0][0]
 
-        for release in release_loop[1:]:
-            update_date = eTree.SubElement(dates, 'date')
-            update_date.set('dateType', 'Updated')
-            update_date.text = release[0]
-            update_date.set('dateInformation', release[1])
+    for release in release_loop[1:]:
+        update_date = eTree.SubElement(dates, 'date')
+        update_date.set('dateType', 'Updated')
+        update_date.text = release[0]
+        update_date.set('dateInformation', release[1])
 
-        resource_type.set('resourceTypeGeneral', 'Dataset')
+    resource_type.set('resourceTypeGeneral', 'Dataset')
 
-        return xml_tostring(root, encoding="UTF-8")
+    xml_string = xml_tostring(root, encoding="UTF-8")
+    b64_bytes = base64.b64encode(xml_string)
+    return b64_bytes.decode('UTF-8')
+
+
+def create_or_update_doi(entry, timeout=1):
+    """ Assign the metadata, then update the URL/release the DOI if not yet created. """
+
+    timeout = timeout * 2
+    try:
+        doi = determine_doi(entry)
+        full_data = {
+            "data": {
+                "id": doi,
+                "type": "dois",
+                "attributes": {
+                    "event": "hide",
+                    "doi": doi,
+                    "url": determine_entry_url(entry),
+                    "xml": get_entry_metadata(entry)
+                }
+            }
+        }
+        url = f"{base_url}/dois/{doi}"
+        r = requests.put(url, json=full_data, auth=(username, password),
+                         headers={'Content-Type': 'application/vnd.api+json'})
+        r.raise_for_status()
+
+        logging.info("Created or updated entry: %s" % entry)
+    except requests.HTTPError as e:
+        logging.warning("A HTTP exception occurred for entry %s: %s" % (entry, e))
+        logging.info("Trying again...")
+        if timeout <= 128:
+            time.sleep(timeout)
+            return create_or_update_doi(entry, timeout)
+        else:
+            logging.error('Multiple attempts to assign entry %s failed.' % entry)
+    except IOError as err:
+        logging.exception("Entry %s not loaded in the DB: %s" % (entry, err))
+    finally:
+        time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -236,9 +248,6 @@ if __name__ == "__main__":
     h1.addFilter(InfoFilter())
     logger.addHandler(h1)
 
-    # Fetch entries
-    cur = psycopg2.connect(user='ets', host='localhost', database='ETS').cursor()
-
     entries = []
     if options.database == "metabolomics":
         entries = requests.get("https://api.bmrb.io/v2/list_entries?database=metabolomics").json()
@@ -249,12 +258,20 @@ if __name__ == "__main__":
         entries.extend(requests.get("https://api.bmrb.io/v2/list_entries?database=metabolomics").json())
 
     if options.days != 0:
-        cur.execute("""
+        with psycopg2.connect(user='ets', host='ets.bmrb.io', database='ETS') as conn:
+            cur = conn.cursor()
+            cur.execute("""
 SELECT bmrbnum
 FROM entrylog
-WHERE status LIKE 'rel%%'
+WHERE status LIKE 'w%%'
   AND accession_date > current_date - INTERVAL '%d days';""" % options.days)
-        entries = [str(x[0]) for x in cur.fetchall()]
+            entries = [str(x[0]) for x in cur.fetchall()]
+
+            cur.execute("""
+SELECT *
+FROM entrylog
+WHERE status LIKE 'awd%';""")
+            withdrawn = cur.fetchall()
 
     if options.override:
         entries = [options.override]
@@ -262,8 +279,10 @@ WHERE status LIKE 'rel%%'
     if options.dry_run:
         for en in entries:
             logger.setLevel(logging.INFO)
-            logger.info("Create or update: %s" % EZIDSession().determine_doi(en))
+            logger.info("Create or update: %s" % determine_doi(en))
         sys.exit(0)
 
     with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
-        p.map(assign_id, entries)
+        p.map(create_or_update_doi, entries)
+
+    #withdraw(entries[0])
