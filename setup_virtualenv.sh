acf36ca16d4078e3d7f61cac763bdea2a468039e
Jon Wedell <jon@jonwedell.com>
2025-02-20 10:23:14 -0600

    Use a standard named `venv` virtual environment.

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-python3 -m venv ${SCRIPT_DIR}/env
-source "${SCRIPT_DIR}"/env/bin/activate
+python3 -m venv "${SCRIPT_DIR}"/venv
+source "${SCRIPT_DIR}"/venv/bin/activate
 pip3 install -r requirements.txt
